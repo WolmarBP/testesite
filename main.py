25316996ce54669f7fc517df18716b396138bfee
WolmarBP <123498014+WolmarBP@users.noreply.github.com>
2023-06-30 11:44:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Meu site")
+
+st.subheader("Meu primeiro site")
+st.title("Pesquisar Visitantes")
+st.write("apPsmecol 1.0 [Clique aqui](https://psmecol.com/)")
